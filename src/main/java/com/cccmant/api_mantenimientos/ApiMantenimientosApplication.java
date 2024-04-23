5bd2ff253f07febdd7b67f83494c3afd8a790af0
Neyris Bohorquez <113575840+NeyrisBH@users.noreply.github.com>
2024-04-23 12:04:47 -0500

    login

--- src/main/java/com/cccmant/api_mantenimientos/ApiMantenimientosApplication.java
+++ src/main/java/com/cccmant/api_mantenimientos/ApiMantenimientosApplication.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -28,27 +27,28 @@ public class ApiMantenimientosApplication {
 
 		@SuppressWarnings("deprecation")
 		protected void configure(HttpSecurity http) throws Exception {
-			http.addFilterAfter(new JwtFilter(), UsernamePasswordAuthenticationFilter.class)
-					.authorizeRequests(authorizeRequests -> authorizeRequests
-							.requestMatchers(HttpMethod.POST, "/api/token").permitAll()
-							.requestMatchers(HttpMethod.POST, "/api/v1/tecnicos").permitAll()
-							.requestMatchers(HttpMethod.GET, "/api/v1/ubicaciones").hasAuthority("Admin")
-							.anyRequest().authenticated())
-					.csrf(csrf -> csrf.disable())
-					.cors(Customizer.withDefaults());
+			http
+				.authorizeRequests(authorizeRequests -> authorizeRequests
+						.requestMatchers(HttpMethod.POST, "/api/token").permitAll()
+						.requestMatchers(HttpMethod.POST, "/api/v1/tecnicos").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/v1/usuarios").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/v1/ubicaciones").hasAuthority("Admin")
+						.anyRequest().authenticated())
+				.csrf(csrf -> csrf.disable())
+				.cors(Customizer.withDefaults());
 		}
-	}
 
-	@Bean
-	public CorsConfigurationSource configurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD"));
-		configuration.setExposedHeaders(Arrays.asList("Authorization"));
-		configuration.setAllowedOrigins(Arrays.asList("*"));
+		@Bean
+		public CorsConfigurationSource configurationSource() {
+			CorsConfiguration configuration = new CorsConfiguration();
+			configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+			configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD"));
+			configuration.setExposedHeaders(Arrays.asList("Authorization"));
+			configuration.setAllowedOrigins(Arrays.asList("*"));
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+			return source;
+		}
 	}
 }
--- src/main/java/com/cccmant/api_mantenimientos/controller/UbicacionController.java
+++ src/main/java/com/cccmant/api_mantenimientos/controller/UbicacionController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +26,6 @@ public class UbicacionController {
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> crearUbicacion(@RequestBody Ubicacion ubicacion) {
         Optional<Ubicacion> ubicaciionCrear = servicio.consultarPorId(ubicacion.getCodigo());
         if (ubicaciionCrear.isPresent()) {
