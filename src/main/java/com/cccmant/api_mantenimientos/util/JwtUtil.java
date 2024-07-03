41fe574f6b007db159a9eea279d6dc341e23d5e1
Neyris Bohorquez <113575840+NeyrisBH@users.noreply.github.com>
2024-07-03 07:21:27 -0500

    time token

--- src/main/java/com/cccmant/api_mantenimientos/util/JwtUtil.java
+++ src/main/java/com/cccmant/api_mantenimientos/util/JwtUtil.java
@@ -31,7 +31,7 @@ public class JwtUtil {
                         .map(GrantedAuthority::getAuthority)
                         .collect(Collectors.toList()))
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1800000))
+                .setExpiration(new Date(System.currentTimeMillis() + 2700000))
                 .signWith(SECRET_KEY)
                 .compact();
         return jwt;
